f2cc80390f1f0629b2ccf400fd670d9e128fd017
Avaer Kazmer <a@modules.io>
2023-05-16 19:57:39 -0400

    Dead code cleanup

--- app.py
+++ app.py
@@ -117,29 +117,6 @@ def get_boxes(img_file: UploadFile = File(...)):
     masks = remove_overlaps(masks, 0.01)
     print("Filtered masks to", len(masks))
 
-    # # get captions
-    # print("Getting captions")
-    # captions = get_segment_captions(image, masks)
-    # print("Got", len(captions), "captions")
-
-    # # show results
-    # fig, ax = plt.subplots(figsize=(20, 20))
-
-    # for mask in masks[:int(len(masks))]:
-    #     x, y, w, h = mask['bbox']
-    #     x, y, w, h = int(x), int(y), int(w), int(h)
-    #     x = x - 10 if x - 10 > 0 else 0
-    #     y = y - 10 if y - 10 > 0 else 0
-    #     w = w + 10 if x + w + 10 < image.shape[1] else w
-    #     h = h + 10 if y + h + 10 < image.shape[0] else h
-
-    #     # draw bounding box and caption using matplotlib
-    #     rect = patches.Rectangle((x, y), w, h, linewidth=1, edgecolor='b', facecolor='none')
-    #     ax.add_patch(rect)
-
-    #     # put image caption in the center left of the bounding box
-    #     ax.text(x, y + h/2, mask['caption'], fontsize=12, color='blue', horizontalalignment='left', verticalalignment='center')
-
     # convert masks to json
     json_masks = []
     for mask in masks:
